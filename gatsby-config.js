76e02d8ac9b06c157bd28b9e78017d2e871db3e6
Jeff Holst <jeffholst@gmail.com>
2020-01-08 08:11:28 -0600

    Branded config files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  pathPrefix: "/Windows-Terminal-Customizer",
+  pathPrefix: "/windows-terminal-customizer-docs",
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `WTC Docs`,
+    description: `Windows Terminal Customizer Documentation.`,
+    author: `jeffholst@gmail.com`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
